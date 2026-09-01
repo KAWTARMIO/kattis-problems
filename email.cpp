931d8788820f58d7c2f96a45043c882b171b59df
KAWTARMIO <bestnaji89@gmail.com>
2026-09-01 10:29:26 +0100

    push problem 4 solution

--- email.cpp
+++ email.cpp
@@ -6,19 +6,5 @@ int main() //kattis problem 5
     for (int r = 1; r <= 20; r++) {
         std::cout << "HIP HIP!" << std::endl;
     }
-//kattis problem 4
-int a, b;
-    cin >> a;
-    cin >> b;
- 
-    if (a > b) {
-        cout << "MAGA!" << endl;
-    }
-    if (a < b) {
-        cout << "FAKE NEWS!" << endl;
-    }
-    if (a == b) {
-        cout << "WORLD WAR 3!" << endl;
-    }
     return 0;
 }
