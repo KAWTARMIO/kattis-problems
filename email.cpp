5ea2e323909ed9f2cad07d748003a31617d28e6e
KAWTARMIO <bestnaji89@gmail.com>
2026-09-01 10:22:11 +0100

    update problem 4

--- email.cpp
+++ email.cpp
@@ -6,6 +6,19 @@ int main() //kattis problem 5
     for (int r = 1; r <= 20; r++) {
         std::cout << "HIP HIP!" << std::endl;
     }
-
+//kattis problem 4
+int a, b;
+    cin >> a;
+    cin >> b;
+ 
+    if (a > b) {
+        cout << "MAGA!" << endl;
+    }
+    if (a < b) {
+        cout << "FAKE NEWS!" << endl;
+    }
+    if (a == b) {
+        cout << "WORLD WAR 3!" << endl;
+    }
     return 0;
 }
