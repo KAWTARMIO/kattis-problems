b975965af90492fd88c737bc188c6e62c3ca0e4e
KAWTARMIO <bestnaji89@gmail.com>
2026-09-01 10:17:56 +0100

    Add Kattis problem 5 solution

---
+++ email.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() //kattis problem 5
+{
+    for (int r = 1; r <= 20; r++) {
+        std::cout << "HIP HIP!" << std::endl;
+    }
+
+    return 0;
+}
