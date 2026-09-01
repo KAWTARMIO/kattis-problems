764de49f2a70d9646ea86c263b58abafc1105e0a
KAWTARMIO <bestnaji89@gmail.com>
2026-09-01 10:32:57 +0100

    push problem 4 solution

---
+++ buttons.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()//kattis reoblem 4
+{
+    int a, b;
+    cin >> a;
+    cin >> b;
+
+    if (a > b) {
+        cout << "MAGA!" << endl;
+    }
+    if (a < b) {
+        cout << "FAKE NEWS!" << endl;
+    }
+    if (a == b) {
+        cout << "WORLD WAR 3!" << endl;
+    }
+
+    return 0;
+}
